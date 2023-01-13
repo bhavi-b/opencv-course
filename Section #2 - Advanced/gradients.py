ae5f7294835a2c7431991174e3f7959600ed2002
bhavi-b <bhavishaybharti@gmail.com>
2023-01-13 18:00:40 +0530

    Finished the basic tut

--- Section #2 - Advanced/gradients.py
+++ Section #2 - Advanced/gradients.py
@@ -3,7 +3,7 @@
 import cv2 as cv
 import numpy as np
 
-img = cv.imread('../Resources/Photos/park.jpg')
+img = cv.imread('D:\SDS\opencv-course\Resources\Photos\cats.jpg')
 cv.imshow('Park', img)
 
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)

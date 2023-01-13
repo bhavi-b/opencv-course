c99f1e10de40d5a83d5ea98f7deecf3fc46cf2dc
bhavi-b <bhavishaybharti@gmail.com>
2023-01-13 15:02:37 +0530

    Finally doin it

--- Section #1 - Basics/basic_functions.py
+++ Section #1 - Basics/basic_functions.py
@@ -27,7 +27,7 @@ eroded = cv.erode(dilated, (7,7), iterations=3)
 cv.imshow('Eroded', eroded)
 
 # Resize
-resized = cv.resize(img, (500,500), interpolation=cv.INTER_CUBIC)
+resized = cv.resize(img, (500,500), interpolation=cv.INTER_CUBIC) #linear cubic area
 cv.imshow('Resized', resized)
 
 # Cropping
